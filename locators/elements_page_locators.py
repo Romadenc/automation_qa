afe2379d14af502e3c45bcca25d828da8df5275d
Ramadan <ramadan@gmail.com>
2024-04-26 00:17:05 +0200

    some locators

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+
+    FULL_NAME = (By.CSS_SELECTOR,"input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR,"input[id='userEmail']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR,"input[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR,"input[id='permanentAddress']")
+    SUBMIT = (By.CSS_SELECTOR,"button[id='submit']")
