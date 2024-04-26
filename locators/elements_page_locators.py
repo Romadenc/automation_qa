01df74123fa1750c888110ea4f030889532d0511
Ramadan <ramadan@gmail.com>
2024-04-26 23:54:52 +0200

    added some fields

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -2,9 +2,16 @@ from selenium.webdriver.common.by import By
 
 
 class TextBoxPageLocators:
+    #form fields
 
-    FULL_NAME = (By.CSS_SELECTOR,"input[id='userName']")
-    EMAIL = (By.CSS_SELECTOR,"input[id='userEmail']")
-    CURRENT_ADDRESS = (By.CSS_SELECTOR,"input[id='currentAddress']")
-    PERMANENT_ADDRESS = (By.CSS_SELECTOR,"input[id='permanentAddress']")
-    SUBMIT = (By.CSS_SELECTOR,"button[id='submit']")
+    FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
+    SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
+
+    #created ffrom
+    CREATED_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
+    CREATED_EMAIL = (By.CSS_SELECTOR, '#output #email')
+    CREATED_CURRENT_ADDRESS =(By.CSS_SELECTOR, '#output #currentAddress')
+    CURRENT_PERMANENT_ADDRESS = (By.CSS_SELECTOR, '#output #permanentAddress')
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,10 +1,21 @@
 import time
 from pages.base_page import  BasePage
+from pages.elements_page import TextBoxPage
+from selenium.webdriver.common.by import By
+
+
+class TestElements:
+    class TestTextBox:
+
+        def test_tex_box(self, driver):
+            text_box_page = TextBoxPage(driver,'https://demoqa.com/text-box')
+            text_box_page.open()
+            text_box_page.fill_all_fields()
+            time.sleep(4)
 
 
 
-def test(driver):
-    page = BasePage(driver, 'https://www.google.com/')
-    page.open()
-    time.sleep(3)
+
+
+
 
