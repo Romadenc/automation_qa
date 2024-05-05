a616f6a733ed2c02809944090e1157ce917061ac
Romadenc <152557194+Romadenc@users.noreply.github.com>
2024-05-05 21:10:41 +0200

    Data generate (#1)
    * some generators
    
    * add generators and implicit wait,so submit button are clickable 100%
    
    ---------
    
    Co-authored-by: Ramadan <ramadan@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ from selenium import webdriver
 @pytest.fixture(scope='function')
 def driver():
     driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
     driver.maximize_window()
-    time.sleep(2)
+    time.sleep(3)
     yield driver
     driver.quit()
