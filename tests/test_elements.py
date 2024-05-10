381f6b39032a40d083d5df2151c0b8c08a2b5716
Ramadan <ramadan@gmail.com>
2024-05-10 18:58:56 +0200

    added some data to data.py added class WebTablePage to elements_page.py added class WebTablePageLocators to elements_page_locators.py added some data to generator.py added class TestWebTable to test_elements.py

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,5 +1,8 @@
+import random
+import time
+
 import pytest
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -39,5 +42,23 @@ class TestElements:
 
             output_impressive = radio_button_page.get_output_result()
             assert output_impressive == 'Impressive',f"Expected 'Yes',but got {output_impressive}"
+
+    class TestWebTable:
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
+            assert new_person in table_result
+
+
+        def test_web_table_search_person(self,driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+            web_table_page.search_some_person(key_word)
+            table_result = web_table_page.check_search_person()
+            assert key_word in table_result,"the person was not found in the table"
+
 
 
